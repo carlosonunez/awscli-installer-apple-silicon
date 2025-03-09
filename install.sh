fa342e563205286ee0e92cd888d15351f7c110cd
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2025-03-09 15:21:04 -0500

    [install] require semver and greater than 2.x
    Signed-off-by: Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -17,12 +17,29 @@ help_requested() {
 }
 
 get_arg_value() {
+  _ensure_semver() {
+    grep -Eiq '^2\.[0-9]{1,}\.[0-9]{1,}$' <<< "$1"
+  }
+  _requested_version_greater_than_v2() {
+    grep -Eiq '^2' <<< "$1"
+  }
   key="${1,,}"
   shift
   while test "$#" -gt 0
   do
     grep -Eiq "^${key}$" <<< "${1,,}" || { shift; shift; continue; }
-    echo "$2"
+    val="${2%.}"
+    if ! _requested_version_greater_than_v2 "$val"
+    then
+      >&2 echo "ERROR: This script only supports v2 of the AWS CLI. (You requested '$val'.)"
+      exit 1
+    fi
+    if ! _ensure_semver "$val"
+    then
+      >&2 echo "ERROR: Version '$val' is not valid (should be something like 2.x.x)"
+      exit 1
+    fi
+    echo "$val"
     return 0
   done
 }
@@ -34,4 +51,3 @@ then
 fi
 
 version=$(get_arg_value "(-v|--version)" "$@")
-echo "Version: $version"
